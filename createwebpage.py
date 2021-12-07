d9c1c14acd8d136465378a16dcb4856f168eddae
Tiina <koodiit@gmail.com>
2021-12-07 21:37:01 +0200

    Readfiles.py works in Cloud, Createtc.py locally

---
+++ createwebpage.py
@@ -0,0 +1,20 @@
+from google.cloud import storage
+
+def create_indexpage(request):
+    """picks the interesting bits from the request and saves them into the bucket as index.html"""
+    text = request["teksti"]
+    picture = request["kuva"]
+    
+    indexpage = "<h1>{}</h1>\n<center><img src={}></center>".format(text, picture)
+    print("index.html written")
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket("pyyttonitesti")
+    blob = bucket.blob("index.html")
+
+    blob.upload_from_string(indexpage)
+    return f'Success!'
+
+if __name__ == "__main__":
+    hep=({"teksti": "Dogs sleep for an average of 10 hours per day.", "kuva": "https://random.dog/6edac66e-c0de-4e69-a9d6-b2e6f6f9001b.jpg"})
+    create_indexpage(hep)
---
+++ readfiles.py
@@ -0,0 +1,24 @@
+"""into requirements.txt 
+pytest==5.4.2
+python-dotenv==0.17.1
+google-cloud-storage==1.43.0"""
+
+import json
+import urllib.request
+from  google.cloud import storage
+from flask import jsonify
+
+def avaa_tiedostot(request):
+    tiedosto = urllib.request.urlopen("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1")
+    tiedot_data = json.loads(tiedosto.read())
+    parametri_data_teksti = ""
+    for d in sorted(tiedot_data, key=lambda x: x['fact']):
+        parametri_data_teksti += d['fact']
+    
+    
+    tiedosto = urllib.request.urlopen("https://random.dog/woof.json")
+    tiedot_data = json.loads(tiedosto.read())
+    parametri_data_kuva = tiedot_data['url']
+    output = {"teksti":parametri_data_teksti, "kuva":parametri_data_kuva}
+    json_object = jsonify(output)
+    return json_object
